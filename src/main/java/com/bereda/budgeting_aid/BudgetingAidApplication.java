b3b382fa86f04e865e768d72fcbb357c060ffaaa
bermateusz <beredamateusz@gmail.com>
2021-01-10 11:36:10 +0100

    init

---
+++ src/main/java/com/bereda/budgeting_aid/BudgetingAidApplication.java
@@ -0,0 +1,13 @@
+package com.bereda.budgeting_aid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingAidApplication.class, args);
+	}
+
+}
